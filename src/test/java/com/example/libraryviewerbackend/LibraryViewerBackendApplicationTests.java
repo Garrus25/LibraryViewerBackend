745b37b4355eae98f56f64df3a5fddae43b1ed3d
MateuszR <mateusz.rysula@gmail.com>
2023-12-25 17:38:16 +0100

    init

---
+++ src/test/java/com/example/libraryviewerbackend/LibraryViewerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryviewerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryViewerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
