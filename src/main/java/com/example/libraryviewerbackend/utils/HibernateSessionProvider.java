c89532c9bfbe248d8e44154225077aaa7f74d0b9
MateuszR <mateusz.rysula@gmail.com>
2024-04-06 13:29:42 +0200

    Added support for metamodel, added config bean to enable cors

--- src/main/java/com/example/libraryviewerbackend/utils/HibernateSessionProvider.java
+++ src/main/java/com/example/libraryviewerbackend/utils/HibernateSessionProvider.java
@@ -5,9 +5,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class HibernateUtil {
+public class HibernateSessionProvider {
 
-    private HibernateUtil() {}
+    private HibernateSessionProvider() {}
     private static final String DATABASE_NAME = "libraryviewerdb";
 
     public static Session getHibernateSession() {
