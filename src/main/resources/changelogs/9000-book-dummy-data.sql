2a0710338639971cb62d87cfe9ecfeda87a2b7cd
MateuszR <mateusz.rysula@gmail.com>
2024-05-04 09:57:26 +0200

    added dummy book data

---
+++ src/main/resources/changelogs/9000-book-dummy-data.sql
@@ -0,0 +1,67 @@
+insert into books (isbn, author_id, title, description, cover_name, publish_date)
+values(
+       1892950960,
+       1,
+       'Stephen King: Uncollected Unpublished',
+       'dummy description',
+       '275631-352x500.jpg',
+       '2006-10-01');
+
+insert into books (isbn, author_id, title, description, cover_name, publish_date)
+values(
+       11111111111,
+       1,
+       'Infernaliana',
+       '1. "Cztery talizmany" – orientalna powiastka filozoficzna o braciach, z których trzech otrzymało od potężnego dżina osobiste magiczne talizmany. Pierwszy zapewniał nieskończone bogactwo, drugi powodzenie u kobiet, trzeci natomiast znajomość wszystkich chorób oraz metod ich leczenia. Czy jednak to, co jest pożądane przez wszystkich ludzi, na pewno przynosi szczęście? Istnieje bowiem jeszcze jeden, czwarty talizman...',
+       '1146705-352x500.jpg',
+       '2003-11-01');
+
+insert into books (isbn, author_id, title, description, cover_name, publish_date)
+values(11111111112,
+       1,
+       'Antologia horroru medycznego ',
+       'dummy description',
+       '1093014-352x500.jpg',
+       '2023-10-01');
+
+insert into books (isbn, author_id, title, description, cover_name, publish_date)
+values(11111111113,
+       1,
+       'Sfora',
+       'Pies to najlepszy przyjaciel człowieka. Lecz porzucony potrafi zmienić się w najzacieklejszego wroga. Sfora Davida Fishera – powieść wpisująca się w popularny w latach 70. nurt horrorów o zemście natury – dowodzi tego w sposób bezdyskusyjny!
+        Rodzina Hardmanów udaje się w zimowe odwiedziny do dziadków zamieszkałych na odciętej od świata Wyspie Burrowsa. Wkrótce przyjdzie im walczyć o życie: letnicy, którzy opuścili wyspę z końcem sezonu urlopowego, pozostawili tu na pastwę losu swoje psy. Wola przetrwania surowej zimy skłania domowe zwierzęta do zorganizowania się w drapieżną sforę. Stado głodnych i rozwścieczonych bestii staje się utrapieniem mieszkańców, a burza śnieżna odcina wyspę od kontaktu ze stałym lądem. Zaczyna się prawdziwy horror!',
+       '1152649-352x500.jpg',
+       '2024-04-29');
+
+insert into books (isbn, author_id, title, description, cover_name, publish_date)
+values(9788368069068,
+       2,
+       'Diabeł zabierze was do domu ',
+       'Ojciec. Mąż. Płatny zabójca.
+        Stracił wszystko. Zabije, żeby wszystko odzyskać.
+
+        To nigdy nie była praca jak każda inna. Mario został płatnym zabójcą, bo tylko w ten sposób mógł spłacić rachunki za leczenie ciężko chorej córeczki.
+
+        A potem dostał propozycję: ostatnia robota, po której albo na zawsze wygrzebie się z biedy, albo skończy z kulą w głowie. Człowiek imieniem Juanca chce okraść kartel narkotykowy na dwa miliony dolarów i potrzebuje pomocy.',
+       '1151415-352x500.jpg',
+       '2024-04-26');
+
+insert into books (isbn, author_id, title, description, cover_name, publish_date)
+values(9788383611754,
+       2,
+       ' Drapieżcy ',
+       'Czy ten przerażający dom istnieje dzisiaj, czy wczoraj… czy jutro?
+
+        Czy we wszystkich tych wymiarach jednocześnie?
+
+        Fortyfoot House – zaniedbany wiktoriański sierociniec na wyspie Wight – kryje w sobie mroczną tajemnicę. Przed ponad stu laty zmarli tam w niewyjaśnionych okolicznościach wszyscy wychowankowie – ponad sześćdziesięcioro dzieci. Okoliczni mieszkańcy nadal unikają rozmów na temat zabytkowego domu i omijają go z daleka. Coś podstępnego i przerażającego czai się w jego wnętrzu.',
+       '1134280-352x500.jpg',
+       '1993-01-01 ');
+
+insert into books (isbn, author_id, title, description, cover_name, publish_date)
+values(9788368005066,
+       2,
+       'Grzechòt',
+       'W ciepłe wakacje młody Jakub przedziera się przez ogrodzenie na działkę zwariowanego sąsiada, o którym krążą legendy. W jego starym sadzie znajduje szopę, a w niej pozbawione kół czarne auto. Utracona kropla krwi to według Kuby niewielka cena za błyskotkę ukradzioną z pakamery. Do czasu… Metaliczny smak juchy obudzi bestię w czarnym pojeździe.',
+       '1150973-352x500.jpg',
+       '2024-04-24');
