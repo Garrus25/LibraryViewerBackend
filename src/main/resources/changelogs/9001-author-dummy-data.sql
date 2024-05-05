a00c87efce3071d9733e9118ada015ac6c76455d
MateuszR <mateusz.rysula@gmail.com>
2024-05-05 13:56:52 +0200

    Added dummy author data

---
+++ src/main/resources/changelogs/9001-author-dummy-data.sql
@@ -0,0 +1,55 @@
+insert into authors (author_id, name, surname, description, addition_date, picture_name)
+values (1,
+        'Stephen',
+        'King',
+        'Stephen Edwin King – amerykański pisarz specjalizujący się w literaturze grozy. W przeszłości wydawał książki pod pseudonimem Richard Bachman, raz jako John Swithen. Jego książki rozeszły się w nakładzie przekraczającym 350 milionów egzemplarzy, co czyni go jednym z najbardziej poczytnych pisarzy na świecie',
+        '2023-01-01',
+        '1020127-352x500.jpg');
+
+insert into authors (author_id, name, surname, description, addition_date, picture_name)
+values (2,
+        'Cixin',
+        'Liu',
+        'Jedna z największych gwiazd chińskiej i światowej fantastyki naukowej. Zanim w pełni poświęcił się karierze pisarskiej, przez wiele lat pracował jako inżynier w elektrowni. Opublikował ponad 20 antologii i powieści z gatunku hard science fiction. Otrzymał liczne nagrody Yinhe i Xingyun, najbardziej prestiżowe wyróżnienia dla chińskojęzycznych twórców fantastyki. Jego trylogia „Wspomnienie o przeszłości Ziemi” została uznana za największe w ostatnich latach wydarzenie w światowej SF. W 2015 roku za Problem trzech ciał, pierwszy tom cyklu, Liu zdobył nagrodę Hugo dla najlepszej powieści, po raz pierwszy w historii przyznaną autorowi nieanglojęzycznemu. Książka znalazła się także w finale nagród Nebula, Prometeusza, Locusa i Johna W. Campbella. Mark Zuckerberg, twórca Facebooka, uznał ją za jedną z najbardziej wartych przeczytania książek roku, a ówczesny prezydent Barack Obama zabrał ją na urlop na Hawajach.',
+        '2023-01-02',
+        '787961-352x500.jpg');
+
+insert into authors (author_id, name, surname, description, addition_date, picture_name)
+values (3,
+        'Rebecca',
+        'Yarros',
+        'Rebecca Yarros to 42-letnia amerykańska pisarka mieszkająca w Kolorado.
+        Znana przede wszystkim jako autorka tytułu "Fourth Wing. Czwarte Skrzydło" oraz serii "Renegaci" i "Rzeczy, których nie dokończyliśmy". Kocha kawę, czekoladę i dietę Paleo. Jest niepoprawną romantyczką, mężatką, matką oraz współzałożycielką organizacji non profit.',
+        '2023-01-03',
+        '1127350-352x500.jpg');
+
+insert into authors (author_id, name, surname, description, addition_date, picture_name)
+values (4,
+        'Delia',
+        'Owens',
+        'DELIA OWENS jest współautorką trzech międzynarodowych bestsellerów literatury faktu opisujących jej życie jako badaczki afrykańskiej fauny i flory. Zdobyła prestiżową nagrodę „John Burroughs Award for Nature Writing”. Publikuje artykuły w czasopismach „Nature”, „African Journal of Ecology” oraz „International Wildlife”. Mieszka w Idaho. „Gdzie śpiewają raki” to jej debiutancka powieść.https://www.deliaowens.com/',
+        '2024-01-01',
+        '711834-352x500.jpg');
+
+insert into authors (author_id, name, surname, description, addition_date, picture_name)
+values (5,
+        'Sławomir',
+        'Gortych',
+        'Autor bestsellerowych kryminałów, których akcja rozgrywa się w Karkonoszach, i wielki miłośnik tych gór. Od najmłodszych lat zgłębia ich sekrety, związane z nimi legendy i historie. Poza tym stomatolog z wykształcenia i pasji. W wolnych chwilach prowadzi blog „Zagubiony w Karkonoszach”. Inspirację do swojej twórczości czerpie z wędrówek po górach oraz rozmów z ich mieszkańcami.
+Jego pierwsza powieść, Schronisko, które przestało istnieć, zdobyła tytuł Książki Roku 2022 w kategorii Debiut w największym plebiscycie czytelniczym w Polsce organizowanym przez serwis Lubimyczytać. Rok później w tym samym konkursie kontynuacja serii, Schronisko, które przetrwało, została Książką Roku 2023 w kategorii Kryminał, Sensacja, Thriller. Oba tomy doceniono także tytułem Najlepszej Górskiej Książki Roku 2023 na Festiwalu Literatury Górskiej w Lądku-Zdroju. W uzasadnieniu jury konkursu zaznaczyło, że powieści przełamują tabu dotyczące odkrywania powojennej historii Karkonoszy widzianej w perspektywie polsko-niemiecko-czeskiej historii ich mieszkańców.
+Autor został również uhonorowany Karkonoską Nagrodą Literacką przyznawaną przez polsko-niemieckie jury za wybitne osiągnięcia w dziedzinie literatury, kultury i sztuki Śląska, które szczególnie służą duchowi humanizmu i porozumienia.',
+        '2024-03-04',
+        '1141563-352x500.jpg');
+
+
+insert into authors (author_id, name, surname, description, addition_date, picture_name)
+values (6,
+        'Remigiusz',
+        'Mróz',
+        'Polski pisarz, autor powieści kryminalnych oraz cyklu publicystycznego „Kurs pisania”.
+Ukończył z wyróżnieniem Akademię Leona Koźmińskiego w Warszawie, gdzie uzyskał stopień naukowy doktora nauk prawnych.
+Laureat Nagrody Czytelników Wielkiego Kalibru z 2016 roku za powieść pt. „Kasacja”. W 2017 roku ujawnił się jako Ove Løgmansbø, autor kolejnych trzech powieści.
+Jego książki były nominowane w Plebiscycie Książka Roku lubimyczytać.pl w kategorii kryminał, sensacja, thriller. Otrzymał dotąd dwie statuetki.
+Seria z Joanną Chyłką stała się podstawą serialu „Chyłka. Zaginięcie” w reżyserii Łukasza Palkowskiego z Magdaleną Cielecką w roli głównej. Wykupiono także prawa do ekranizacji innych serii autora - z komisarzem Forstem i cyklu „W kręgach władzy”.http://www.remigiuszmroz.pl',
+        '2024-04-01',
+        '1145716-352x500.jpg');
